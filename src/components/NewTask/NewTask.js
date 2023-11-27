d1156be1f51cd326077ba67a35e6c460bf787eff
nghabd <dr_nageh@yahoo.com>
2023-11-28 01:08:31 +0300

    new use

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,40 +1,32 @@
 import { useState } from "react";
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState(null);
+	const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+	const createTak = (taskText, taskData) => {
+		const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+		const createdTask = { id: generatedId, text: taskText };
+		console.log(createdTask.hasOwnProperty());
+
+		props.onAddTask(createdTask);
+	};
 
 	const enterTaskHandler = async (taskText) => {
-		setIsLoading(true);
-		setError(null);
-		try {
-			const response = await fetch(
-				"https://max-react-http-d63c5-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
-				{
-					method: "POST",
-					body: JSON.stringify({ text: taskText }),
-					headers: {
-						"Content-Type": "application/json",
-					},
-				}
-			);
+		sendTaskRequest(
+			{
+				url: "https://max-react-http-d63c5-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
 
-			if (!response.ok) {
-				throw new Error("Request failed!");
-			}
-
-			const data = await response.json();
-			const generatedId = data.name; // firebase-specific => "name" contains generated id
-			const createdTask = { id: generatedId, text: taskText };
-			console.log(createdTask.hasOwnProperty());
-
-			props.onAddTask(createdTask);
-		} catch (err) {
-			setError(err.message || "Something went wrong!");
-		}
-		setIsLoading(false);
+				method: "POST",
+				body: JSON.stringify({ text: "taskText" }),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			},
+			createTak.bind(null, taskText)
+		);
 	};
 
 	return (
